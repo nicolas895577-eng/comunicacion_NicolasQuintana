618c695063915884eafc2597bba4e227a6f9cebc
Nicolas <Nicolas0@gmail.com>
2025-11-21 22:17:44 -0500

    Segundo Commit comunicacion entre componentes correcion de errores

--- src/app/services/navigation.ts
+++ src/app/services/navigation.ts
@@ -10,19 +10,19 @@ export interface MenuOption {
 })
 export class NavigationService {
 
-  // Signal para el título actual (Home, Search, HTML)
+  
   currentTitle = signal<string>('Home');
 
-  // Signal para la lista de opciones del menú
+ 
   currentOptions = signal<MenuOption[]>([
-    // Datos iniciales (Home)
+    
     { label: 'Sena presentation', path: 'sena' },
     { label: 'Ficha 3066474', path: 'ficha/3066474' },
     { label: 'Ficha 3333333', path: 'ficha/3333333' },
     { label: 'Cuarta opción' }
   ]);
 
-  // Método que llamarán los componentes para cambiar la info
+  
   updateData(title: string, options: MenuOption[]) {
     this.currentTitle.set(title);
     this.currentOptions.set(options);
